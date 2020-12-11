3375820a9c0b7c9868fc65374066607633f48dc9
Young Jeong <youngjeong46@gmail.com>
2020-12-11 11:19:23 -0800

    removed AWS CDK; using Serverless Framework

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # serverless-notification-service
-A simple, serverless app that sends subscriber with notification using AWS Lambda and AWS SNS. 
-This is built using AWS CDK.
+
+A simple, serverless app that sends subscriber with notification using AWS Lambda and AWS SNS. This is built using Serverless Framework.
--- bin/serverless-notification-service.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from '@aws-cdk/core';
-import { ServerlessNotificationServiceStack } from '../lib/serverless-notification-service-stack';
-
-const app = new cdk.App();
-new ServerlessNotificationServiceStack(app, 'ServerlessNotificationServiceStack');
